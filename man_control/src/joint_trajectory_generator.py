cd813130aff3a75b7a0135c5ee8c540ce72a1d66
Devdutt Subhasish <devdutt.subhasish@gmail.com>
2021-07-28 11:28:28 +0530

    Added doc for joint_trajectory_generator.py

--- man_control/src/joint_trajectory_generator.py
+++ man_control/src/joint_trajectory_generator.py
@@ -3,7 +3,12 @@
 """
 Created on Mon May 25 19:15:21 2020
 
-@author: kuttus09
+@author: dsubhasish
+
+The class defined here can be used as a template for any trajectory generator
+in the joint space. In this specific case, a sinusoidal trajectory is generated,
+with the desired joint space position, velocity and acceleration sent to the
+joint space controller by means of the topic /theta_desired.
 """
 
 
@@ -19,17 +24,25 @@ sin=np.sin
 class Sine_Traj_Gen(object):
     """
     Generates sinusoidal joint space trajectory and publishes it to /theta_desired
+    
+    This generator produces a sinusoidal trajectory between the joint space position
+    with all angles set to 0 and all angles set to 90 degrees. A suitable angular 
+    frequency of oscillation is chosen so as to remain within the control
+    bandwidth
+
+    Attributes
+    ----------
+    rate : rospy.Rate(200)
+        To set rate of publishing messages as 200 Hz.
+    trajd : np.ndarray of shape (18,1)
+        Desired trajectory
+    traj : Float64MultiArray
+        The computed trajectory will be transferred to this message type for publishing
+    trajectory_pub : rospy.Publisher
+        Publisher of desired joint trajectory topic (/theta_desired).
     """
     
     def __init__(self):
-        """
-        Initializes the Sine_Traj_Gen Object.
-
-        Returns
-        -------
-        None.
-
-        """
         
         self.rate=rospy.Rate(200)#rate at which trajectory should be published. Currently set to 200Hz
         self.trajd=np.zeros((18,1))#stores the desired sinusoidal joint space trajectory
@@ -38,6 +51,17 @@ class Sine_Traj_Gen(object):
 
         
     def send_trajectory(self):
+        """
+        Runs the desired trajectory computation and publishing loop. At every
+        iteration, takes in the current simulation time, computes the desired
+        joint space position, velocity and acceleration, and then publishes it
+        over to the topic /theta_desired.
+
+        Returns
+        -------
+        None.
+
+        """
         omega=0.25*2*pi#angular frequency
         t0=rospy.get_time()#initial time
         theta_max=(pi/2)*np.ones((6,1))
