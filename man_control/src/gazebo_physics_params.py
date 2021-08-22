c78f5ad75aa4a79faf46502fe72aeb306f72e7d7
Devdutt Subhasish <devdutt.subhasish@gmail.com>
2021-08-22 18:32:30 +0530

    Added extended physics reconfigure feature

--- man_control/src/gazebo_physics_params.py
+++ man_control/src/gazebo_physics_params.py
@@ -38,25 +38,58 @@ class Gazebo_Physics:
         self.gravity.y=y
         self.gravity.z=z
         
-    def set_physics_ODE(self):
+    def set_physics_ODE(self, auto_disable:bool=False,precon_iters:int=0,iters:int=100,w:float=1.3,rms_error_tol:float=0.0,surface_layer:float=0.001,max_correcting_vel:float=100,cfm:float=0.0,erp:float=0.5,max_contacts:int=100):
+        """
+        Sets the physics engine to ODE and configures its parameters.
+
+        Parameters
+        ----------
+        auto_disable : bool, optional
+            DESCRIPTION. The default is False.
+        precon_iters : int, optional
+            DESCRIPTION. The default is 0.
+        iters : int, optional
+            DESCRIPTION. The default is 100.
+        w : float, optional
+            DESCRIPTION. The default is 1.3.
+        rms_error_tol : float, optional
+            DESCRIPTION. The default is 0.0.
+        surface_layer : float, optional
+            DESCRIPTION. The default is 0.001.
+        max_correcting_vel : float, optional
+            DESCRIPTION. The default is 100.
+        cfm : float, optional
+            DESCRIPTION. The default is 0.0.
+        erp : float, optional
+            DESCRIPTION. The default is 0.5.
+        max_contacts : int, optional
+            DESCRIPTION. The default is 100.
+
+        Returns
+        -------
+        None.
+
+        """
         self.physics=ODEPhysics()
-        self.physics.auto_disable_bodies=False
-        self.physics.sor_pgs_precon_iters=0
-        self.physics.sor_pgs_iters=100
-        self.physics.sor_pgs_w=1.3
-        self.physics.sor_pgs_rms_error_tol= 0.0
-        self.physics.contact_surface_layer= 0.001
-        self.physics.contact_max_correcting_vel= 100.0
-        self.physics.cfm= 0.0
-        self.physics.erp= 0.2
-        self.physics.max_contacts= 100
+        self.physics.auto_disable_bodies=auto_disable
+        self.physics.sor_pgs_precon_iters=precon_iters
+        self.physics.sor_pgs_iters=iters
+        self.physics.sor_pgs_w=w
+        self.physics.sor_pgs_rms_error_tol= rms_error_tol
+        self.physics.contact_surface_layer= surface_layer
+        self.physics.contact_max_correcting_vel= max_correcting_vel
+        self.physics.cfm= cfm
+        self.physics.erp= erp
+        self.physics.max_contacts= max_contacts
     
     def change_physics(self):
         self.set_physics(self.time_step,self.max_update_rate,self.gravity,self.physics)
     
 if __name__=="__main__":
     max_update_rate=int(sys.argv[1])
+    time_step=float(sys.argv[2])
     g_phys=Gazebo_Physics()
     g_phys.set_max_update_rate(max_update_rate)
+    g_phys.set_time_step(time_step)
     g_phys.set_physics_ODE()
     g_phys.change_physics()
